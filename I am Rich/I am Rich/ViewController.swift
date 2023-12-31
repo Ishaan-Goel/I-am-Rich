8a803e558304c0c63d6633021f98b589c0352a06
Ishaan-Goel <139787489+Ishaan-Goel@users.noreply.github.com>
2023-12-31 15:37:06 -0800

    Add files via upload

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,15 @@
+//  ViewController.swift
+//  I am Rich
+//  Created by Ishaan Goel on 8/26/23.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
